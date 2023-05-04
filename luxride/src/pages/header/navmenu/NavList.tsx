cf7f1fef9ce9d51321bc181fe4e43866fc6fdd58
Patrik Mitro <mitropatrik@gmail.com>
2023-05-04 02:15:48 +0200

    optimize mobile responsivity

--- luxride/src/pages/header/navmenu/NavList.tsx
+++ luxride/src/pages/header/navmenu/NavList.tsx
@@ -1,18 +1,15 @@
 import React, { FC } from "react";
 import styled, { css } from "styled-components";
-import DownArrow from "../../../assets/images/DownArrow.png";
-import DownArrowBlack from "../../../assets/images/DownArrowBlack.png";
+
+import { menuItems } from "./navmenudata/navMenuItemsData";
+
 import Text from "../../../components/text/Text";
 import { Theme } from "../../../styles/theme.styles";
-
+import NavMenuItems from "./NavMenuItems";
 interface Props {
   isOpened: boolean;
+  screenWidth: boolean;
 }
-
-const Image = styled.img`
-  align-self: center;
-  cursor: pointer;
-`;
 const UnorderedList = styled.ul`
   display: flex;
   padding-left: 32px;
@@ -27,99 +24,46 @@ const UnorderedList = styled.ul`
     }
   }
 `;
-const List = styled.li`
-  display: flex;
-  gap: 8px;
-  padding: 8px 16px;
-  border-radius: 40px;
-  @media only screen and (max-width: 1100px) {
-    justify-content: space-between;
-  }
-  cursor: pointer;
-  &:hover {
-    background-color: ${Theme.colors.grey};
-  }
-`;
+
 const NavMenuWrapper = styled.div<Props>`
-  transition: 0.5s ease-in-out;
   @media only screen and (max-width: 1100px) {
     position: fixed;
-    transform: translate(100%);
+    transition: transform 0.5s ease-in-out;
     top: 80px;
+    z-index: 99;
+    left: 0;
     ${(props) =>
-      props.isOpened &&
-      css`
-        transform: translate(0);
-        left: 0;
-      `}
+      props.isOpened
+        ? css`
+            transform: translate(0);
+          `
+        : css`
+            transform: translate(100%);
+          `}
     width: 100%;
+    height: 100vh;
+    background-color: ${Theme.colors.white};
     flex-direction: column;
   }
 `;
-const NavList = ({ isOpened }: Props) => {
+const NavList = ({ isOpened, screenWidth }: Props) => {
   return (
-    <NavMenuWrapper isOpened={isOpened}>
+    <NavMenuWrapper isOpened={isOpened} screenWidth={screenWidth}>
+      {" "}
+      {/* NEchapem tomu */}
       <UnorderedList>
-        <List>
-          <Text
-            fontColor={isOpened ? Theme.colors.black : Theme.colors.white}
-            fontSize={isOpened ? "24px" : "14px"}
-            fontWeight="500"
-          >
-            Home
-          </Text>
-          <Image src={isOpened ? DownArrowBlack : DownArrow} alt="down arrow" />
-        </List>
-        <List>
-          <Text
-            fontColor={isOpened ? Theme.colors.black : Theme.colors.white}
-            fontSize={isOpened ? "24px" : "14px"}
-            fontWeight="500"
-          >
-            Pages
-          </Text>
-          <Image src={isOpened ? DownArrowBlack : DownArrow} alt="down arrow" />
-        </List>
-        <List>
-          <Text
-            fontColor={isOpened ? Theme.colors.black : Theme.colors.white}
-            fontSize={isOpened ? "24px" : "14px"}
-            fontWeight="500"
-          >
-            Our Fleet
-          </Text>
-          <Image src={isOpened ? DownArrowBlack : DownArrow} alt="down arrow" />
-        </List>
-        <List>
-          <Text
-            fontColor={isOpened ? Theme.colors.black : Theme.colors.white}
-            fontSize={isOpened ? "24px" : "14px"}
-            fontWeight="500"
-          >
-            Services
-          </Text>
-          <Image src={isOpened ? DownArrowBlack : DownArrow} alt="down arrow" />
-        </List>
-        <List>
-          <Text
-            fontColor={isOpened ? Theme.colors.black : Theme.colors.white}
-            fontSize={isOpened ? "24px" : "14px"}
-            fontWeight="500"
-          >
-            Blog
-          </Text>
-          <Image src={isOpened ? DownArrowBlack : DownArrow} alt="down arrow" />
-        </List>
-        <List>
-          <Text
-            fontColor={isOpened ? Theme.colors.black : Theme.colors.white}
-            fontSize={isOpened ? "24px" : "14px"}
-            fontWeight="500"
-          >
-            Contact
-          </Text>
-          <Image src={isOpened ? DownArrowBlack : DownArrow} alt="down arrow" />
-        </List>
+        {menuItems.map(
+          (menu: { title: string; url: string }, index: number) => {
+            return (
+              <NavMenuItems
+                key={index}
+                title={menu.title}
+                screenWidth={screenWidth}
+                url={menu.url}
+              />
+            );
+          }
+        )}
       </UnorderedList>
     </NavMenuWrapper>
   );
