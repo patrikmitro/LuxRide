62643f6a91284316f11d2188a604c2d6117b1613
Patrik Mitro <mitropatrik@gmail.com>
2023-05-04 02:15:05 +0200

    add navMenuItemsData

---
+++ luxride/src/pages/header/navmenu/navmenudata/navMenuItemsData.ts
@@ -0,0 +1,56 @@
+export const menuItems: { title: string; url: string }[] = [
+  {
+    title: "Home",
+    url: "/c",
+  },
+  {
+    title: "Services",
+    url: "/c",
+  },
+  {
+    title: "About",
+    url: "/c",
+  },
+];
+
+/* export const navMenuItemsData = [
+  {
+    title: "Pages",
+    to: "/",
+    submenu: [
+      {
+        title: "About",
+        to: "/About",
+      },
+      {
+        title: "Contact",
+        to: "/Contact",
+      },
+      {
+        title: "Login",
+        to: "/",
+      },
+      {
+        title: "Register",
+        to: "/",
+      },
+      {
+        title: "Faq",
+        to: "/Faq",
+      },
+      {
+        title: "Testimonials",
+        to: "/Testimonials",
+      },
+      {
+        title: "404 Page",
+        to: "/404",
+      },
+      {
+        title: "Coming Soon",
+        to: "/Coming-soon",
+      },
+    ],
+  },
+];
+ */
