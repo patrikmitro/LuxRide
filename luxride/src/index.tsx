ad88de15d5603ed9c8bab7b99f3affc71286091a
Patrik Mitro <mitropatrik@gmail.com>
2023-05-04 02:13:28 +0200

    import react-router-dom

--- luxride/src/index.tsx
+++ luxride/src/index.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 
+import Header from "./pages/header/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<h1>Main</h1>} />
+        <Route path="/c" element={<h1>Yo</h1>} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
