903ff032abb1030d1ea8aad70db644aefa92bc21
Patrik Mitro <mitropatrik@gmail.com>
2023-05-29 13:56:13 +0200

    Start Home page

--- luxride/src/pages/home/Home.tsx
+++ luxride/src/pages/home/Home.tsx
@@ -3,30 +3,15 @@ import React from "react";
 import { Theme } from "../../styles/theme.styles";
 
 import Image from "../../components/image/Image";
+import Title from "../../components/text/Title";
+import { MainWrapper } from "./Home.styles";
 
-import {
-  MainWrapper,
-  LandingPageWrapper,
-  ImageWrapper,
-  Overlay,
-} from "./Home.styles";
+import LandingPage from "./landingpage/LandingPage";
 
-import LandingPageBackground from "../../assets/images/MercedesBackGround.png";
 const Home = () => {
   return (
     <MainWrapper>
-      <LandingPageWrapper>
-        <ImageWrapper>
-          <Image
-            src={LandingPageBackground}
-            alt="Mercedes"
-            width="100%"
-            height="780px"
-            objectFit="cover"
-          />
-          <Overlay backGround={Theme.colors.black} />
-        </ImageWrapper>
-      </LandingPageWrapper>
+      <LandingPage />
     </MainWrapper>
   );
 };
