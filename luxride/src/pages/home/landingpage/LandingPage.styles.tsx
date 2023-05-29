446c767822141501e61fea5f4da6d75e1dba24f1
Patrik Mitro <mitropatrik@gmail.com>
2023-05-29 13:56:25 +0200

    Add LandingPage styles

---
+++ luxride/src/pages/home/landingpage/LandingPage.styles.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+interface HomeProps {
+  backGround?: string;
+  backGroundImage?: string;
+}
+
+export const MainWrapper = styled.main``;
+
+export const LandingPageWrapper = styled.div<HomeProps>`
+  position: relative;
+  height: 780px;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(${({ backGroundImage }) => backGroundImage});
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 46px;
+`;
+
+export const ImageWrapper = styled.div`
+  position: relative;
+`;
+export const Overlay = styled.div<HomeProps>`
+  z-index: 1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${({ backGround }) => backGround};
+  opacity: 0.52;
+`;
+
+export const HeadingWrapper = styled.div`
+  width: 1170px;
+  margin: 0 auto;
+  z-index: 2;
+  position: relative;
+`;
