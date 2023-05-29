cb6c67874c4d9fcff89100d4e6293f41bf443de3
Patrik Mitro <mitropatrik@gmail.com>
2023-05-29 13:54:36 +0200

    Add Title component

---
+++ luxride/src/components/text/Title.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+
+interface HeadingProps {
+  level: 1 | 2 | 3 | 4 | 5 | 6;
+  children: React.ReactNode;
+  fontWeight?: string;
+  fontSize?: string;
+  fontColor?: string;
+  textDecoration?: string;
+}
+
+const Heading = ({
+  level,
+  children,
+  fontColor,
+  textDecoration,
+  fontSize,
+  fontWeight,
+}: HeadingProps) => {
+  const StyledHeading = styled(`h${level}`)<HeadingProps>`
+    font-weight: ${({ fontWeight }) => fontWeight};
+    font-size: ${({ fontSize }) => fontSize};
+    color: ${({ fontColor }) => fontColor};
+  `;
+
+  return (
+    <StyledHeading
+      fontColor={fontColor}
+      fontSize={fontSize}
+      fontWeight={fontWeight}
+      level={level}
+    >
+      {children}
+    </StyledHeading>
+  );
+};
+
+export default Heading;
