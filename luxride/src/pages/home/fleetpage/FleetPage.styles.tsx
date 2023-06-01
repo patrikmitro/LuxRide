da8b8096f9c4268d5f6f4333b9c35b8b6960897c
Patrik Mitro <mitropatrik@gmail.com>
2023-06-01 20:46:51 +0200

    Create FleetPage.styles.tsx

---
+++ luxride/src/pages/home/fleetpage/FleetPage.styles.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const FleetPageWrapper = styled.section`
+  padding: 30px;
+`;
+
+export const FleetPageContainer = styled.div`
+  width: 100%;
+  max-width: 1170px;
+  margin: 90px auto;
+`;
+
+export const FleetTitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const MoreFleetContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`;
