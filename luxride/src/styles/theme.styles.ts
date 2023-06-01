4accb87e0273e66dbc47c96d7158f9f49b2383dd
Patrik Mitro <mitropatrik@gmail.com>
2023-06-01 20:45:10 +0200

    Add new color

--- luxride/src/styles/theme.styles.ts
+++ luxride/src/styles/theme.styles.ts
@@ -4,8 +4,11 @@ export const Theme = {
     red: "#FE5100",
     yellow: "#EFFC53",
     green: "#22C58B",
+    baseBlack: "#181A1F",
     main: "#393938",
     grey: "#333333",
     white: "#FFFFFF",
+    whiteGrey: "#F6F6F6",
+    lightGrey: "#E5E5E5",
   },
 };
