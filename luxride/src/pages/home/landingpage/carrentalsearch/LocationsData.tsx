02c4eda7787613f47aa0399fec0504aed8c98fc0
Patrik Mitro <mitropatrik@gmail.com>
2023-06-01 20:45:56 +0200

    Create LocationsData.tsx

---
+++ luxride/src/pages/home/landingpage/carrentalsearch/LocationsData.tsx
@@ -0,0 +1,18 @@
+export const LocationsData: { value: string; location: string }[] = [
+  {
+    value: "London Heathrow Airport (LHR)",
+    location: "London Heathrow Airport (LHR)",
+  },
+  {
+    value: "London Tower Hill",
+    location: "London Tower Hill",
+  },
+  {
+    value: "Leyton Train Station",
+    location: "Leyton Train Station",
+  },
+  {
+    value: "London Luton Airport (LTN)",
+    location: "London Luton Airport (LTN)",
+  },
+];
