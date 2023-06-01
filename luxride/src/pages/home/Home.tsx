cb4f71d8e4df717f466a83b5d2b864126ae97e5f
Patrik Mitro <mitropatrik@gmail.com>
2023-06-01 20:46:29 +0200

    Add new components to Home

--- luxride/src/pages/home/Home.tsx
+++ luxride/src/pages/home/Home.tsx
@@ -3,11 +3,14 @@ import React from "react";
 import { MainWrapper } from "./Home.styles";
 
 import LandingPage from "./landingpage/LandingPage";
-
+import PartnersPage from "./partnerspage/PartnersPage";
+import FleetPage from "./fleetpage/FleetPage";
 const Home = () => {
   return (
     <MainWrapper>
       <LandingPage />
+      <PartnersPage />
+      <FleetPage />
     </MainWrapper>
   );
 };
