4be35844c562383a61712686822e788d9f8ac7e2
Patrik Mitro <mitropatrik@gmail.com>
2023-06-01 20:47:14 +0200

    Create Paragraph.tsx

---
+++ luxride/src/components/text/Paragraph.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+interface TextProps {
+  children: React.ReactNode;
+  fontWeight?: string;
+  fontSize?: string;
+  fontColor?: string;
+  onClick?: () => void;
+  hover?: string;
+  textDecoration?: string;
+}
+
+export const Paragraph = styled.p<TextProps>`
+  font-weight: ${({ fontWeight }) => fontWeight};
+  font-size: ${({ fontSize }) => fontSize};
+  color: ${({ fontColor }) => fontColor};
+
+  :hover {
+    ${({ hover }) => hover}
+  }
+`;
