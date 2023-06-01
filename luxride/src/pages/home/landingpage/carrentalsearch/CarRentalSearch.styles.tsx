c0a56b4aa121e6ad31be230fdffe283be6760203
Patrik Mitro <mitropatrik@gmail.com>
2023-06-01 20:46:10 +0200

    Update CarRentalSearch.styles.tsx

--- luxride/src/pages/home/landingpage/carrentalsearch/CarRentalSearch.styles.tsx
+++ luxride/src/pages/home/landingpage/carrentalsearch/CarRentalSearch.styles.tsx
@@ -4,21 +4,56 @@ interface Props {
   backGround?: string;
   backGroundIcon?: string;
 }
-
-export const SearchWrapper = styled.div<Props>`
+export const SearchContainer = styled.div<Props>`
   display: flex;
   justify-content: space-between;
   align-items: center;
   border-radius: 100px;
   background-color: ${({ backGround }) => backGround};
-  padding: 18px;
+  padding: 18px 24px;
+  @media only screen and (max-width: 990px) {
+    flex-direction: column;
+    gap: 8px;
+    background-color: transparent;
+    align-items: flex-start;
+    max-width: 500px;
+    padding: 0px;
+    button {
+      width: 100%;
+      justify-content: center;
+    }
+  }
 `;
 
-export const StyledLabel = styled.label`
+export const SearchWrapper = styled.div<Props>`
+  @media only screen and (max-width: 990px) {
+    margin: -30px;
+    padding: 16px;
+    background-color: #fff;
+  }
+`;
+
+export const StyledLabel = styled.label<Props>`
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 4px;
+  img {
+    background-color: #f6f6f6;
+    padding: 16px;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+  @media only screen and (max-width: 990px) {
+    background-color: ${({ backGround }) => backGround};
+    width: 100%;
+
+    justify-content: flex-start;
+    border-radius: 6px;
+    span {
+      display: none;
+    }
+  }
 `;
 export const StyledInput = styled.input<Props>`
   display: flex;
@@ -37,3 +72,10 @@ export const StyledInput = styled.input<Props>`
     cursor: pointer;
   }
 `;
+
+export const StyledSelect = styled.select`
+  border: none;
+  width: 100%;
+`;
+
+export const StyledOption = styled.option``;
